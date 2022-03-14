46fa55abbde1116caba29682b1ec3b3e34009451
Khaleefa <mdcarliey@gmail.com>
2022-03-14 14:44:20 +0100

    initial commmit

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledHeader } from "./Styled/Header.Styled";
+
+function Header() {
+  return (
+    <StyledHeader>
+      <h1>Hubble</h1>
+    </StyledHeader>
+  );
+}
+
+export default Header;
